315404b4bf38d7b7c11ba34f73b1fef0a76421b0
Bilal-Waleed <161094448+Bilal-Waleed@users.noreply.github.com>
2024-04-12 22:49:59 -0700

    To-Do- App

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# To-Do-App-
-This is a to-do app developed using TypeScript
+
+# Command Line TO-DO List Application
+
+## Description
+
+This is a simple command line TO-DO list application built with Node.js. It allows users to manage their tasks by performing various operations such as adding, updating, viewing, and deleting tasks.
+
+## Installation
+
+To use the TO-DO list application, follow these steps:
+
+1. Clone the repository to your local machine:
+
+    ```
+    git clone <repository-url>
+    ```
+
+2. Navigate to the project directory:
+
+    ```
+    cd todo-list-app
+    ```
+
+3. Install the dependencies:
+
+    ```
+    npm install
+    ```
+
+## Usage
+
+To start the application, run the following command in your terminal:
+
+```
+npm start
+```
+
+Follow the prompts to perform the desired operations on your TO-DO list.
+
+## Features
+
+- **Add**: Add a new task to the TO-DO list.
+- **Update**: Update an existing task in the TO-DO list.
+- **View**: View the current status of the TO-DO list.
+- **Delete**: Delete a task from the TO-DO list.
+
+## How to Contribute
+
+Contributions are welcome! If you would like to contribute to this project, you can follow these steps:
+
+1. Fork the repository
+2. Create a new branch (`git checkout -b feature`)
+3. Make your changes
+4. Commit your changes (`git commit -am 'Add new feature'`)
+5. Push to the branch (`git push origin feature`)
+6. Create a new Pull Request
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgements
+
+- This application was built using [Node.js](https://nodejs.org/) and [Inquirer.js](https://www.npmjs.com/package/inquirer).
+- Special thanks to [Bilal Waleed](https://github.com/bilalwaleed) for developing this application.
+
+---
+
+Feel free to modify this template according to your specific project details and preferences. Let me know if you need further assistance!
---
+++ index.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+//1) first asking username  
+//2) todos array
+//3) functions 
+//4) operaions
+//----------------------------ASKING USER NAME-----------------------------------
+const askUserName = async () => {
+    const userName_ans = await inquirer.prompt({
+        name: "usr_name",
+        type: "input",
+        message: chalk.magenta(`\nWhat is Your Good Name:`),
+        validate: (input) => {
+            const trimmedInput = input.trim();
+            if (trimmedInput === "") {
+                return chalk.redBright("Please enter your name.");
+            }
+            else if (!/^[a-zA-Z]+$/.test(trimmedInput)) {
+                return chalk.redBright("Please enter a valid name without numbers.");
+            }
+            return true;
+        },
+    });
+    return userName_ans.usr_name;
+};
+let todos = [];
+async function createTodos(todos) {
+    const userName = await askUserName();
+    //-------------------------TO-DO LIST HEADING -------------------------
+    let appName = chalk.magenta.underline.italic(` Welcome "${userName}" in TO-DO List: `);
+    console.log(`\n\t`, `\t`, `\t`, `${appName}\n`);
+    //-------------------------- MAIN FUNCTION ---------------------------------
+    let useTodo = true;
+    while (useTodo) {
+        let ans = await inquirer.prompt({
+            name: `select`,
+            type: `list`,
+            message: `Select an operation:`,
+            choices: [`Add`, `Update`, `View`, `Delete`]
+        });
+        //------------------------ ADD ITEM ----------------------------------------
+        if (ans.select == `Add`) {
+            let addTodo = await inquirer.prompt({
+                name: `addNew`,
+                type: `input`,
+                message: `Add items...`,
+                validate: (input) => {
+                    if (input.trim() === ``) {
+                        return chalk.redBright("Please enter something.");
+                    }
+                    return true;
+                },
+            });
+            todos.push(addTodo.addNew);
+            console.log(chalk.yellow.underline(`\n\t Item Added:  ${todos.join(` , `)}\n`));
+        }
+        //---------------------------UPDATE ITEM --------------------------------------
+        if (ans.select == `Update`) {
+            let updateTodo = await inquirer.prompt({
+                name: `todo`,
+                type: `list`,
+                message: `Select item for update:`,
+                choices: todos.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                name: `addNew`,
+                type: `input`,
+                message: `Add item for update...`,
+                validate: (input) => {
+                    if (input.trim() === ``) {
+                        return chalk.redBright("Please enter something.");
+                    }
+                    return true;
+                },
+            });
+            let newTodos = todos.filter(item => item !== updateTodo.todo);
+            todos = [...newTodos, addTodo.addNew];
+            console.log(chalk.yellow.underline(`\n\t Item Updated: ${todos.join(` , `)}\n`));
+        }
+        //-----------------------------VIEW ITEM ------------------------------------
+        if (ans.select == `View`) {
+            console.log(chalk.yellow.underline(`\n\t Curent list status: ${todos.join(` - `)}\n`));
+        }
+        //-----------------------------DELETE ITEM -------------------------------------
+        if (ans.select == `Delete`) {
+            let deleteTodo = await inquirer.prompt({
+                name: `todo`,
+                type: `list`,
+                message: `Select item for Delete:`,
+                choices: todos.map(item => item)
+            });
+            let newTodos = todos.filter(val => val !== deleteTodo.todo);
+            todos = [...newTodos];
+            console.log(chalk.redBright.underline(`\n\t Item Deleted: ${deleteTodo.todo}`));
+            console.log(chalk.yellow.underline(`\n\t Remaining Items: ${todos.join(` , `)}\n`));
+        }
+        const confirmation_ans = await inquirer.prompt({
+            name: `user_confirmation`,
+            type: `confirm`,
+            message: chalk.red.bold(`Do you want to use TO-DO list more..? `),
+            default: true,
+        });
+        if (confirmation_ans.user_confirmation == false) {
+            useTodo = false;
+        }
+    }
+    console.log(chalk.yellow.underline("\n\t Thank you for using the TO-DO LIST. Have a great day!"));
+    let develporName = chalk.magenta.underline ` BILAL WALEED `;
+    console.log(chalk.magenta(`\n\t`, `\t`, `\t`, `Developer Name: ${develporName}`));
+    process.exit();
+}
+createTodos(todos);
